fd55f907bfe48ce277a472b1bfba019bceebf9b5
Edward Leung <edward_leung_12@hotmail.com>
2020-08-25 14:03:10 +0100

    added data for autocomplete

--- app.js
+++ app.js
@@ -12,3 +12,21 @@ M.Slider.init(slider, {
   transition: 500,
   interval: 5000,
 });
+
+// Autocomplete (Search Locations)
+const ac = document.querySelector(".autocomplete");
+// Init ac with options
+M.Autocomplete.init(ac, {
+  data: {
+    Africa: null,
+    California: null,
+    Canada: null,
+    Spain: null,
+    Dubai: null,
+    "Hong Kong": null,
+    Ibiza: null,
+    "Los Angeles": null,
+    Sydney: null,
+    Florida: null,
+  },
+});
