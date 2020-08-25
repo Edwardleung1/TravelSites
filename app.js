f1040ba30d3bc028c38170c661393d085bef273c
Edward Leung <edward_leung_12@hotmail.com>
2020-08-25 13:02:58 +0100

    added a slider init

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-//Sidenav
+// Sidenav
 const sideNav = document.querySelector(".sidenav");
-//Init sidenav
+// Init sidenav
 M.Sidenav.init(sideNav, {});
+
+// Slider
+const slider = document.querySelector(".slider");
+// Init slider with options
+M.Slider.init(slider, {
+  indicators: false,
+  height: 500,
+  transition: 500,
+  interval: 5000,
+});
